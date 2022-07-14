2d612b5709d2849aa053367cac0f4b6edee7734e
HoangT <ac5217@student.jamk.fi>
2022-07-14 13:31:16 +0300

    refactoring the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Movie Search React App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchMovie.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Search movie component
+//form with a class of form
+//lable with html="query"
+//input of type text with a name of query and a place holder
+//button class of button and a type of submit
+
+function SearchMovie() {
+  return <div>SearchMovie</div>;
+}
+
+export default SearchMovie;
