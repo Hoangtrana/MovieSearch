480516353237c5e638d63e29b24d818f2276b540
HoangT <ac5217@student.jamk.fi>
2022-07-15 09:21:38 +0300

    search movie fuction, adding movie card component, css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React from "react";
+import SearchMovie from "./SearchMovie";
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">Movie Search React App</h1>
+      <SearchMovie />
     </div>
   );
 }
--- src/SearchMovie.js
+++ src/SearchMovie.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 
 // Search movie component
 //form with a class of form
@@ -6,8 +7,59 @@ import React from "react";
 //input of type text with a name of query and a place holder
 //button class of button and a type of submit
 
-function SearchMovie() {
-  return <div>SearchMovie</div>;
+function SearchMovie(props) {
+  const [query, setQuery] = useState("");
+  // create the state for movies and update that state appropriate
+  const [movies, setMovies] = useState([]);
+  const searchMovies = async (e) => {
+    // state input query, movies
+
+    e.preventDefault();
+    console.log("submitting");
+
+    //const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=01d72bba97e0bc3d62f7efaacdf2c8a8&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+
+      console.log(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card__list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
 }
 
 export default SearchMovie;
